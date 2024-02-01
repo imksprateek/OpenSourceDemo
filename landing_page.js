7a01ad52250a5e5f4bdfdbab9b37365e02ec88a6
imksprateek <ksprateek2004@gmail.com>
2024-02-01 20:23:10 +0530

    Initial Commit

---
+++ landing_page.js
@@ -0,0 +1,5 @@
+const remixButton = document.getElementById("remix-button");
+
+remixButton.addEventListener("click", () => {
+    window.location.href = "index.html"; // Replace with the actual remixing page URL
+});
